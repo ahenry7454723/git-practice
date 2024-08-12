598c318ce4cd98b9737c4d60595199ca57dffa98
aoki_shou <aoki.shou@it-sp.co.jp>
2024-08-12 23:09:10 +0900

    Chapter05.javaクラス追加

---
+++ src/Chapter05.java
@@ -0,0 +1,14 @@
+
+public class Chapter05 {
+	static final int LUCKY_NUMBER = 7;
+
+	public static void main(String[] args) {
+		String name = "青木 渉";
+		System.out.println(name);
+
+		name = "盆休み";
+		System.out.println(name);
+
+		System.out.println(LUCKY_NUMBER);
+	}
+}
