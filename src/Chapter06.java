b3a77016688df0c4d2d0d9359d81be3a8d41e430
aoki_shou <aoki.shou@it-sp.co.jp>
2024-08-13 15:50:05 +0900

    Chapter06.java追加

---
+++ src/Chapter06.java
@@ -0,0 +1,26 @@
+
+public class Chapter06 {
+
+	public static void main(String[] args) {
+		long long1 = 10000000000L;
+		System.out.println(long1);
+
+		double dob1 = 3.14;
+		System.out.println(dob1);
+
+		boolean bool = true;
+		System.out.println(bool);
+
+		Integer int1 = 100;
+		System.out.println(int1);
+
+		Float fl1 = 1.4142F;
+		System.out.println(fl1);
+
+		Character ch = 'う';
+		System.out.println(ch);
+
+		int[] array1 = new int[] { 0, 10, 20, 30, 40 };
+		System.out.println(array1.clone()[3]);
+	}
+}
